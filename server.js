8906624068541b04bb7728eb2636600d053ba444
MartinRenteria <renteriamp96@gmail.com>
2021-12-06 10:00:48 -0800

    Adding express route and adding nodemon dependency

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+app.get("/now", (request, response) => {
+    const date = new Date();
+    response.send(date);
+  });
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
 })
